c36ef29a14e3ec069cf8e9199e25ca7ece1ee9e5
RadoMiami <kguthre@gmail.com>
2021-09-01 13:21:57 -0400

    Fixed copyright information

--- src/main/java/Solution02.java
+++ src/main/java/Solution02.java
@@ -4,8 +4,8 @@ public class Solution02 {
     public static void main(String[] args)
     {
         /*
-         *  UCF COP3330 Fall 2021 Assignment 1 Solution
-         *  Copyright 2021 first_name last_name
+         *  UCF COP3330 Fall 2021 Assignment 2 Solution
+         *  Copyright 2021 Kimari Guthre
          */
         /*Ask what's the input string
         Store said input string
